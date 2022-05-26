7070ab0e76a62654d410f4e8ebf8671a73be7036
Jenni <justlittleoldmejenn@gmail.com>
2022-05-26 16:21:23 -0700

    Cube generator

---
+++ Assets/Scripts/QubeGenerator.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QubeGenerator : MonoBehaviour
+{
+    [SerializeField] private GameObject qubePrefab;
+    [SerializeField] private int layers = 3;
+
+    private void Start()
+    {
+        StartCoroutine(GenerateQube(layers));
+    }
+
+    private IEnumerator GenerateQube(int size)
+    {
+        float qubeSize = qubePrefab.GetComponent<BoxCollider>().size.x;
+
+        for (int i = 0; i < size; i++)
+        {
+            for (int j = 0; j < size; j++)
+            {
+                for (int k = 0; k < size; k++)
+                {
+                    Vector3 pos = new Vector3(k * qubeSize, j * qubeSize, i * qubeSize);
+                    var go = Instantiate(qubePrefab, pos, Quaternion.identity, transform);
+                    yield return new WaitForEndOfFrame();
+                }
+            }
+        }
+    }
+}
